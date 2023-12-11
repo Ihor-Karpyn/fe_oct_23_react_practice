d6158e8b54bb55f420c61f7a2d0a8908faedaec4
Ihor Karpyn <ihor.k@mate.academy>
2023-12-11 11:01:06 +0200

    fx

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,11 @@ import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import { App } from './App';
-import { AppContext, AppContextProvider } from './Context/AppContext';
+import { AppContextProvider } from './Context/AppContext';
 
 ReactDOM.render(
   <AppContextProvider>
-    <App />,
-  </AppContextProvider>
+    <App />
+  </AppContextProvider>,
   document.getElementById('root'),
 );
